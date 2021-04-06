2d57ebd8b8a6daa8f587912153b7c253445c424b
Larissa Varjão <larissasilvavarjao@gmail.com>
2021-04-06 17:41:46 -0300

    feat: add user entity

---
+++ core/user/entity.go
@@ -0,0 +1,25 @@
+package user
+
+/* 1. BANCO DE DADOS - TABELA DE USUARIOS
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    id uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+    first_name varchar(100) NOT NULL,
+    last_name varchar(100),
+		password varchar(255),
+    email varchar(100) UNIQUE NOT NULL,
+		avatar varchar(255)
+);
+
+*/
+
+type User struct {
+	ID    int64     `json:"id"`
+	FirstName  string    `json:"firstName"`
+	LastName  string  `json:"lastName"`
+	Password  string  `json:"password"`
+	Email string `json:"email"`
+	Avatar string `json:"avatar"`
+}
